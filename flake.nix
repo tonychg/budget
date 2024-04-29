ffeb9947191d4062d14e8e9a93c2f817364128fb
tonychg <tonychg7@pm.me>
2024-04-29 14:52:36 +0200

    feat: bootstrap

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Budget manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rust-bin.stable.latest.default
+            rust-analyzer
+            pkg-config
+            openssl
+          ];
+
+          buildInputs = with pkgs; [
+            go-task
+            clippy
+            husky
+          ];
+        };
+      }
+    );
+}
