7160510b54ef5f016aa7172e04b256f4fb4db418
tonychg <tonychg7@pm.me>
2024-04-30 21:54:05 +0200

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,59 @@
 {
-  description = "Budget manager";
+  description = "Budget Manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+        craneLib = crane.lib.${system};
+        bdg = craneLib.buildPackage {
+          src = lib.cleanSourceWith {
+            src = craneLib.path ./.;
+          };
+          strictDeps = true;
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+          buildInputs = with pkgs; [
+            openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
         };
       in
+      with pkgs;
       {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            rust-bin.stable.latest.default
-            rust-analyzer
-            pkg-config
-            openssl
-          ];
+        checks = {
+          inherit bdg;
+        };
+        packages = {
+          default = bdg;
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/bdg";
+          };
+        };
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
 
-          buildInputs = with pkgs; [
+          packages = [
+            rust-analyzer
+            git
             go-task
-            clippy
-            husky
             sops
           ];
         };
-      }
-    );
+      });
 }
